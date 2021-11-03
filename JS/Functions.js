75c96dc41dccedd109984f591da190dd11dcea7b
RheCreate <75132698+RheCreate@users.noreply.github.com>
2021-11-03 18:08:34 -0500

    update

---
+++ JS/Functions.js
@@ -0,0 +1,5 @@
+const myFunction = () => {
+    document.getElementById("first").style.display ='block';
+    document.getElementById("second").style.display ='none'
+    document.getElementById("third").style.display ='none'
+  }
