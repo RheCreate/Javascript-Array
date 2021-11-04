2f77c5f1996c230446dcaa77498d65672f53c3af
RheCreate <75132698+RheCreate@users.noreply.github.com>
2021-11-04 12:20:27 -0500

    Update Functions.js

--- JS/Functions.js
+++ JS/Functions.js
@@ -2,6 +2,7 @@
 document.querySelector("button").addEventListener("click", () => {
   document.body.style.background = 
     `url(https://picsum.photos/500/500?random=${Math.random()})`;
+    document.body.style.backgroundRepeat = "no-repeat";
 });
 
 
